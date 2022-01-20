256dc5cee8cc60816192e7bb0709db6346d91ef3
Florencia A. Castello <68971292+florenciacastello@users.noreply.github.com>
2022-01-20 11:23:32 -0300

    Add files via upload

--- resistance_detection.py
+++ resistance_detection.py
@@ -19,18 +19,12 @@ buscar como leer vcf
 '''
 import argparse
 import pandas as pd
-import defaultdict
+from collections import defaultdict
 import sys
+import math
+from Bio.SeqUtils import seq1
+import json
 
-'''
-import vcf
-def read(variant):
-    reader = vcf.Reader(open(variant))
-    df = pd.DataFrame([vars(r) for r in reader])
-    out = df.merge(pd.DataFrame(df.INFO.tolist()), left_index=True, right_index=True)
-    return out
-o vcfpy
-'''
 
 def resistance_detector(resistance_tb_path):
     """ resistance tb columns: 
@@ -38,7 +32,23 @@ def resistance_detector(resistance_tb_path):
     AAalt/Effect/MultipleMutation/PhylogeneticMarker/Source"""
     genes_truncados=defaultdict(list)
     genes_sinaa=defaultdict(list)
-    resistance_tb=pd.read_csv(resistance_tb_path)
+    resistance_tb=pd.read_csv(resistance_tb_path) 
+    
+    resistance_tb=resistance_tb[resistance_tb['Drug']!='-']
+    def splitpositions(x) :
+        if "-" not in str(x):
+            return int(str(x).split('/')[0])   
+        else:
+            return "-"
+    resistance_tb['NucleotidePosH37']=[  splitpositions(x)
+                                       for x in resistance_tb.NucleotidePosH37
+                                       ]
+    resistance_tb['NucleotidePosGene']=[  splitpositions(x)
+                                       for x in resistance_tb.NucleotidePosGene
+                                       ]
+    #resistance_tb['AApos']=resistance_tb['AApos'].replace('-',None).astype(int)
+    #resistance_tb['NucleotidePosH37']=resistance_tb['NucleotidePosH37'].replace('-',None)
+    #resistance_tb['NucleotidePosGene']=resistance_tb['NucleotidePosGene'].replace('-',None).astype(int)
     for i, r in resistance_tb.iterrows():
         if r.AAalt in ['STOP', 'fs']:
             entry={'row':r, 'Drug':r.Drug}
@@ -48,8 +58,9 @@ def resistance_detector(resistance_tb_path):
             genes_sinaa[r.NucleotidePosH37].append(entry)
     return resistance_tb, genes_truncados, genes_sinaa
 
-def process_vcf(vcf, resistance_tb_path):
+def process_vcf(vcf, resistance_tb_path, out):
      resistance_tb, genes_truncados, genes_sinaa=resistance_detector(resistance_tb_path)
+     resistance_variant=[]
      with open(vcf) as h:
          for line in h:
              if not line.startswith("#"):             
@@ -60,7 +71,7 @@ def process_vcf(vcf, resistance_tb_path):
                 
                 gt_options = {k + 1: v for k, v in alts.items()}
                 gt_options[0] = ref
-                gt_options[99] = "N"                
+                #gt_options[99] = "N"                
                 format_f = vec[8]  # GT:AD:DP:GQ:P
                 gt_index = [i for i, x in enumerate(format_f.split(":")) if x == "GT"]
                 if not gt_index:
@@ -77,12 +88,148 @@ def process_vcf(vcf, resistance_tb_path):
                 ad=sample[ad_index].split(',')
                 dp=sample[dp_index]
                 if gt[0]!='.':
-                    '''asumimos que el segundo es el ALT pero hay que actualzarlo 
-                    para procesar ambos'''
-                    alt=gt_options[int(gt[1])]
-                    variante_ann=procesar_variante(pos, posgen, gen, posaa, aa, alt, \
-                                      resistance_tb, genes_truncados, genes_sinaa)
+                    try:
+                        
+                        '''asumimos que el segundo es el ALT pero hay que actualzarlo 
+                        para procesar ambos'''
+                        alt=gt_options[int(gt[1])]
+                        info=vec[7]
+                        ann=info.split('ANN=')[1].split(';')[0].split(',')[0].split('|')
+                        variant_type=ann[1]
+                        impact=ann[2]
+                        gene_name=ann[3]
+                        lt=ann[4]
+                        gene_variant=ann[9][2:]
+                        if ann[11]:
+                            posgen=ann[11].split('/')[0] 
+                        if ann[10]:
+                            protein_variant=ann[10][2:]
+                            posaa=ann[13].split('/')[0]
+                            #posaa=str(math.ceil(float(posgen)/3))
+                            #print('----', posaa)
+                            variant_reference=['missense_variant', 'stop_gained', 
+                                               'frameshift_variant', 'start_lost']
+                            if set(variant_type.split('&')) & set(variant_reference):
+                                aa_ref=protein_variant.split(posaa)[0]
+                                if posaa in protein_variant:
+                                    aa_alt=protein_variant.split(posaa)[1]
+                                elif protein_variant.endswith('fs'):
+                                    aa_alt='fs'
+                                elif protein_variant.endswith('*'):
+                                    aa_alt='*'
+                                
+                            else:
+                                aa_ref=None
+                                aa_alt=None
+                        else:
+                            protein_variant=None
+                            posaa=None
+                            aa_ref=None
+                            aa_alt=None
+                            
+                        
+                        variante_ann=procesar_variante(pos, ref, posgen, variant_type, impact,  lt, posaa, aa_ref, aa_alt, alt,
+                                          resistance_tb, genes_truncados, genes_sinaa)
+                        if variante_ann: 
+                            variante_ann['AD']={allele:int(ad[index]) for index, allele in gt_options.items()}
+                            '''1-4, 8, 9  ANN=T|missense_variant|MODERATE|dnaA|Rv0001|transcript|Rv0001|protein_coding|1/1|c.71C>T|p.Pro24Leu|71/1524|71/1524|24/507||'''
+                            resistance_variant.append(variante_ann)
+                            #print(variante_ann)
+                    except:
+                        print('Error')
+                        print(line)
+                        print(gt_options, ad)
+                        print(ann)
+                        raise
+         with open (out, "w") as h:
+            json.dump(resistance_variant, h)
+         
+         #print(genes_truncados)
+                        
+                    
+def procesar_variante(pos, ref, posgen, variant_type, impact, lt, posaa, aa_ref, aa_alt, alt, resistance_tb, genes_truncados, genes_sinaa):
+    ''' 1) variante tal cual en genoma (conf alta)
+        2) variante tal cual en prot (conf alta)
+        3) variante tal cual en el gen (conf alta)
+        4) frameshift/STOP de alto impacto en gen truncado (conf alta)
+        5) variante en pos reportada (conf media)
+        6) si esta dentro de un gen de interes y es de impacto moderado (del/ins/NOsinonim)/alto (rumor)
+        7) si esta dentro de un gen con impacto bajo (sinonima/mod codon no expr) (rumor)
+        8) si estan cerca de un gen con bajo/moderado impacto (rumor)'''
+    
+    ''' 1) variante tal cual en genoma (conf alta)'''
+    df_resistance_tb=resistance_tb[(resistance_tb['NucleotidePosH37']==int(pos))&
+                                    (resistance_tb['REF']==ref)&
+                                    (resistance_tb['ALT']==alt)]
+    if len(df_resistance_tb):
+        return {'confidence':'high', 'Drugs':list(set(df_resistance_tb['Drug'])), 'db_entries':
+                [r.to_dict() for _, r in df_resistance_tb.iterrows()]}
+   
+    """2) variante tal cual en prot (conf alta)
+    pensar mejor npara el caso de delec e inserciones de aa"""
+    if aa_alt:        
+        aa_ref=seq1(aa_ref)
+        aa_alt=seq1(aa_alt)
+        df_resistance_tb=resistance_tb[(resistance_tb['AApos']==int(posaa)) &
+                                        (resistance_tb['AAref']==aa_ref)&
+                                        (resistance_tb['AAalt']==aa_alt)]
+        if len(df_resistance_tb):
+            return {'confidence':'high', 'Drugs':list(set(df_resistance_tb['Drug'])), 'db_entries':
+                    [r.to_dict() for _, r in df_resistance_tb.iterrows()]}
+    
+    
+    '''3) variante tal cual en el gen (conf alta)'''
+    df_resistance_tb=resistance_tb[(resistance_tb['NucleotidePosGene']==int(posgen))&
+                                        (resistance_tb['REF']==ref)&
+                                        (resistance_tb['ALT']==alt)]
+    if len(df_resistance_tb):
+        return {'confidence':'high', 'Drugs':list(set(df_resistance_tb['Drug'])), 'db_entries':
+                [r.to_dict() for _, r in df_resistance_tb.iterrows()]}
+    
+    '''4) frameshift/STOP de alto impacto en gen truncado (conf alta)'''
+    trunc=['*', 'fs']
+    if aa_alt in trunc: # agregar star_lost 
+        if lt in genes_truncados:
+            return {'confidence':'high', 'Drugs':list(set([x['Drug'] for x in genes_truncados[lt]])), 'db_entries':
+                    [r for r in genes_truncados[lt]]}    
                 
+    '''5) variante en pos reportada (conf media)'''
+    df_resistance_tb=resistance_tb[(resistance_tb['NucleotidePosH37']==int(pos))&
+                                    (resistance_tb['GeneID']==lt)]
+    if len(df_resistance_tb):
+        if 'synonymous' not in variant_type:
+            return {'confidence':'medium', 'Drugs':list(set(df_resistance_tb['Drug'])), 'db_entries':
+                    [r.to_dict() for _, r in df_resistance_tb.iterrows()]}    
+    df_resistance_tb=resistance_tb[(resistance_tb['NucleotidePosGene']==int(posgen))&
+                                    (resistance_tb['GeneID']==lt)]
+    if len(df_resistance_tb):
+        if 'synonymous' not in variant_type:
+            return {'confidence':'medium', 'Drugs':list(set(df_resistance_tb['Drug'])), 'db_entries':
+                    [r.to_dict() for _, r in df_resistance_tb.iterrows()]}    
+    
+    '''6) si esta dentro de un gen de interes y es de impacto moderado (del/ins/NOsinonim)/alto (rumor)'''
+    rumor_impact=['MODERATE', 'HIGH']
+    if impact in rumor_impact:
+        if lt in set(resistance_tb['GeneID']):
+            df_resistance_tb=resistance_tb[resistance_tb['GeneID']==lt]
+            return {'confidence':'low', 'Drugs':list(set(df_resistance_tb['Drug'])), 'db_entries':
+                    [r.to_dict() for _, r in df_resistance_tb.iterrows()]}  
+            
+    '''7) si esta dentro de un gen con impacto bajo (sinonima/mod codon no expr) '''
+    if 'LOW' in impact:
+        if lt in set(resistance_tb['GeneID']):
+            df_resistance_tb=resistance_tb[resistance_tb['GeneID']==lt]
+            return {'confidence':'low', 'Drugs':list(set(df_resistance_tb['Drug'])), 'db_entries':
+                    [r.to_dict() for _, r in df_resistance_tb.iterrows()]}  
+
+    '''8) si estan cerca de un gen con bajo/moderado impacto (rumor)'''
+    rumor_impact=['MODIFIER', 'LOW']
+    if 'upstream_gene_variant' in variant_type:
+        if impact in rumor_impact:
+            if lt in set(resistance_tb['GeneID']):
+                df_resistance_tb=resistance_tb[resistance_tb['GeneID']==lt]
+                return {'confidence':'low', 'Drugs':list(set(df_resistance_tb['Drug'])), 'db_entries':
+                            [r.to_dict() for _, r in df_resistance_tb.iterrows()]} 
 
 def main():
     parser = argparse.ArgumentParser(description='')
@@ -91,9 +238,12 @@ def main():
     parser.add_argument('-o','--out', help= 'Directorio donde se guarda los \
                         archivos de salida', default='./out_resistance')
     parser.add_argument('-sa', '--sample', help= '', default=None)
-    parser.add_argument('-va', '--vcf_ann', help= '', default='variant.ann.vcf')
+    parser.add_argument('-va', '--vcf_ann', help= '', default='./out_data/variant.ann.vcf')
     parser.add_argument('-r', '--reference', help= '', default='andytb.csv')
+    
     args=parser.parse_args()
+    vcf_procesado=process_vcf(args.vcf_ann, args.reference, args.out)
+    
     
 
 
